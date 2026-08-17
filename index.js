2e4706561a9b0a5a62cac87b8775b51cbe12a53f
aman18111625 <amangupta326519@gmail.com>
2026-08-17 19:27:33 +0530

    ssr handling with ejs

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const urlRoutes = require("./routes/url");
 const { connectToDatabase } = require("./connect");
+const ejs = require("ejs");
+const path = require("path");
+const staticRouter = require("./routes/staticRouter");
+
 const app = express();
 const PORT = 3000;
 
@@ -12,9 +16,15 @@ connectToDatabase("mongodb://localhost:27017/url-shortener")
   console.error("Error connecting to the database:", error);
 });
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/url", urlRoutes);
+
+app.use("/", staticRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
